7a70df0dd051536de0aaa3c26997219546e7d376
Pedro Bibiloni <pedro@bibiloni.es>
2020-05-14 09:17:35 +0200

    Fix plots and prints

--- run.py
+++ run.py
@@ -1,13 +1,16 @@
 import pandas
-from pandas.tools import plotting
+from pandas import plotting
 from scipy.stats import ttest_1samp, ttest_ind, ttest_rel, wilcoxon, bartlett, kstest, shapiro
 
+import matplotlib.pyplot as plt
+
 data = pandas.read_csv('data/brain_size.csv', sep=';', na_values=".")
-print('Medidas: f{data.shape}')
-print('Columnas: f{data.columns}')
+print(f'Medidas: {data.shape}')
+print(f'Columnas: {data.columns}')
 
 plotting.scatter_matrix(data[['Weight', 'Height', 'MRI_Count']])
 plotting.scatter_matrix(data[['PIQ', 'VIQ', 'FSIQ']])
+plt.show()
 
 female_viq = data[data['Gender'] == 'Female']['VIQ']
 male_viq = data[data['Gender'] == 'Male']['VIQ']
